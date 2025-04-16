be976cb52739de55b19af47e404e55e78eb62615
Shannon <blackskiesdancer@gmail.com>
2025-04-16 23:25:26 +1000

    day-one notes

---
+++ day-one.py
@@ -0,0 +1,14 @@
+# Hello World & variables
+print("Hello World")
+
+age = 25
+name = "Shannon"
+colour = "Blue"
+
+print("My name is", name, "I am", age, "and my favourite colour is", colour)
+
+# Changing variables
+colour = "Pink"
+print("Now my favourite colour is", colour)
+
+
