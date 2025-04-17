8f7834ca0c2d39fe34ce00a8d1a95c1f904888ee
Shannon <blackskiesdancer@gmail.com>
2025-04-17 23:37:03 +1000

    file names updated

---
+++ day-3.py
@@ -0,0 +1,18 @@
+  # Lists === Array
+# fruits = ["apple", 'banana', "lemon"]
+#.append() === .push()
+
+# ------------------------------------------------------
+  #        Prompts
+# Prompt 1: Grocery List
+# Ask the user 3 things to buy from the store
+# Add them to a list
+# Print the final shopping list
+
+needs = []
+
+def groceryList (need):
+  needs = input("1st thing?")
+  print("Your shopping list has:", needs)
+
+groceryList()
