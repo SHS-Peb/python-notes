17df53589392d4daa995934001ee645d03fa6663
unknown <blackskiesdancer@gmail.com>
2025-04-20 12:03:04 +1000

    day-3 lists, loops, functions prompts

--- day-3.py
+++ day-3.py
@@ -9,10 +9,49 @@
 # Add them to a list
 # Print the final shopping list
 
-needs = []
+shopping = []
 
-def groceryList (need):
-  needs = input("1st thing?")
-  print("Your shopping list has:", needs)
+shopping.append(input("1st thing?"))
+shopping.append(input("2nd thing?"))
+shopping.append(input("3rd thing?"))
 
-groceryList()
+print("Your shopping list has:", shopping)
+# --------------------------
+#  Prompt 2: Count to 10
+# Write a loop that prints numbers 1 to 10 using range()
+
+for i in range(1, 11):
+  print(i)
+
+# -----------------
+#  Prompt 3: Playlist Maker
+# Ask the user to name 5 songs they love. Store them in a list. Then:
+#     Print: “Your playlist:”
+#     Print each song on a new line
+
+playlist = []
+playlist.append(input("1st song you love?"))
+playlist.append(input("2nd song you love?"))
+playlist.append(input("3rd song you love?"))
+playlist.append(input("4th song you love?"))
+playlist.append(input("5th song you love?"))
+
+for playlist in playlist:
+  print(playlist)
+
+
+# Bonus: Random Choice
+
+# Let the user add items to a list of snacks. When they’re done, randomly pick one snack from the list to “eat”.
+import random
+snacks = []
+
+def snackQ ():
+  question = input("Would you like to add a snack?")
+  while question == "yes":
+    snacks.append(input("What snack would you add?"))
+    question = input("Would you like to add a snack?")
+  else:
+    print(f"Your random snack is {random.choice(snacks)}")
+
+snackQ()
