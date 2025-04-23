f1bf367ef9fcc0d2fb19cf315bf573807f1f26bf
unknown <blackskiesdancer@gmail.com>
2025-04-23 22:40:57 +1000

    time notes if added

--- day-5.py
+++ day-5.py
@@ -1,5 +1,14 @@
 from datetime import datetime
-now = datetime.now()
 
-print("Today's date is:", now.date())
-print("The time is:", now.strftime("%H:%M:%S"))
+now = datetime.now()
+hour = now.hour
+
+if hour < 12:
+    greeting = "Good morning!"
+elif hour < 18:
+    greeting = "Good afternoon!"
+else:
+    greeting = "Good evening!"
+
+print(greeting)
+print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
